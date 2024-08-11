5c3797c9c8652f7ec580b11dba0587c0678d50de
Justin <justin@Justins-MacBook-Pro.local>
2024-08-11 23:08:42 +0900

    Use ittrator adapters to make code more consise.

--- src/lib.rs
+++ src/lib.rs
@@ -17,24 +17,35 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(contents: &'a str, query: &str) -> Vec<&'a str> {
-    let mut result = vec![];
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line.trim())
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+
+    // Before learning about itterator adapters...
+    // let mut result = vec![];
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         result.push(line.trim())
+    //     }
+    // }
+    // result
 }
 
 pub fn search_case_insensitive<'a>(contents: &'a str, query: &str) -> Vec<&'a str> {
-    let mut result = vec![];
-    let query = query.to_lowercase();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line.trim())
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
+
+    // let mut result = vec![];
+    // let query = query.to_lowercase();
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         result.push(line.trim())
+    //     }
+    // }
+    // result
 }
 
 pub struct Config {
@@ -44,13 +55,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let args: Vec<String> = env::args().collect();
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string."),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn:t get a file path."),
+        };
+        // if args.len() < 3 {
+        //     return Err("Not enough arguments");
+        // }
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
